44f029f43cb0b12c27239079ee9f0bc17a7263d9
semenov <semenov@skeeks.com>
2019-08-15 00:36:06 +0300

    dev

--- src/I18NDbComponent.php
+++ src/I18NDbComponent.php
@@ -57,50 +57,53 @@ class I18NDbComponent extends I18N
      */
     public static function handleMissingTranslation(MissingTranslationEvent $event)
     {
+        $cache = \Yii::$app->cache;
+        $key = $event->category.$event->message.$event->language;
+        $messages = $cache->get($key);
 
-        \Yii::info("@DB: {$event->category}.{$event->message} FOR LANGUAGE {$event->language} @", static::class);
+        if ($messages === false) {
+            \Yii::info("@DB: No cache {$event->category}.{$event->message} FOR LANGUAGE {$event->language} @", static::class);
 
-        $driver = \Yii::$app->getDb()->getDriverName();
-        $caseInsensitivePrefix = $driver === 'mysql' ? 'binary' : '';
-        $sourceMessage = SourceMessage::find()
-            ->where('category = :category and message = '.$caseInsensitivePrefix.' :message', [
-                ':category' => $event->category,
-                ':message'  => $event->message,
-            ])
-            ->with('messages')
-            ->one();
+            $driver = \Yii::$app->getDb()->getDriverName();
+            $caseInsensitivePrefix = $driver === 'mysql' ? 'binary' : '';
+            $sourceMessage = SourceMessage::find()
+                ->where('category = :category and message = '.$caseInsensitivePrefix.' :message', [
+                    ':category' => $event->category,
+                    ':message'  => $event->message,
+                ])
+                ->with('messages')
+                ->one();
 
-        if (!$sourceMessage) {
-            \Yii::info("@WRITE TO DB: {$event->category}.{$event->message} FOR LANGUAGE {$event->language} @", static::class);
+            if (!$sourceMessage) {
+                \Yii::info("@WRITE TO DB: {$event->category}.{$event->message} FOR LANGUAGE {$event->language} @", static::class);
 
-            $sourceMessage = new SourceMessage();
-            $sourceMessage->setAttributes([
-                'category' => $event->category,
-                'message'  => $event->message,
-            ], false);
+                $sourceMessage = new SourceMessage();
+                $sourceMessage->setAttributes([
+                    'category' => $event->category,
+                    'message'  => $event->message,
+                ], false);
 
-            $sourceMessage->save(false);
+                $sourceMessage->save(false);
+            }
+            /**
+             * @var $message Message
+             */
+            //print_r($sourceMessage->messages);
+            if ($sourceMessage->messages) {
+                $messages = ArrayHelper::map($sourceMessage->messages, "language", "translation");
+            } else {
+                $messages = [];
+            }
+
+            $cache->set($key, $messages);
         }
 
-        //$sourceMessage->initMessages();
-        //$sourceMessage->saveMessages();
-
-        /*$messages = $sourceMessage->messages;
-
-        if (isset($messages[\Yii::$app->sourceLanguage])) {
-            $message = $messages[\Yii::$app->sourceLanguage];
-            $message->id = $sourceMessage->id;
-            $message->translation = $sourceMessage->message;
-            $message->save(false);
-        }*/
-
-        /**
-         * @var $message Message
-         */
-        $message = ArrayHelper::getValue($sourceMessage->messages, \Yii::$app->language);
+        $message = ArrayHelper::getValue($messages, \Yii::$app->language);
 
         if ($message) {
-            $event->translatedMessage = $message->translation;
+            $event->translatedMessage = $message;
         }
+
+
     }
 }
